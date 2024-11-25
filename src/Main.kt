6396d9cad10429524c89b9581ffaadc525e98631
Aspron <124681044+Aspron@users.noreply.github.com>
2024-11-25 18:28:18 +0100

    Commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,12 @@
 fun main() {
-    println("Introduce la primera palabra:")
+    println("Escribe la primera palabra:")
     val palabra1 = readLine()?.trim() ?: ""
-    println("Introduce la segunda palabra:")
+    println("Escribe la segunda palabra:")
     val palabra2 = readLine()?.trim() ?: ""
 
-    if (sonAnagramas(palabra1, palabra2)) {
+    val resultado = sonAnagramas(palabra1, palabra2)
+
+    if (resultado) {
         println("Las palabras \"$palabra1\" y \"$palabra2\" son anagramas.")
     } else {
         println("Las palabras \"$palabra1\" y \"$palabra2\" no son anagramas.")
@@ -13,7 +15,12 @@ fun main() {
 
 fun sonAnagramas(palabra1: String, palabra2: String): Boolean {
 
-    if (palabra1.equals(palabra2, ignoreCase = true)) return false
+    if (palabra1.equals(palabra2, ignoreCase = true)) {
+        return false
+    }
 
-    return palabra1.lowercase().toCharArray().sorted() == palabra2.lowercase().toCharArray().sorted()
+    val palabra1Ordenada = palabra1.lowercase().toCharArray().sorted()
+    val palabra2Ordenada = palabra2.lowercase().toCharArray().sorted()
+
+    return palabra1Ordenada == palabra2Ordenada
 }
