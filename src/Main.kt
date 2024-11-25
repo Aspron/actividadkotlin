c89daaef26aa46a2bca79f59edd16c8462503da8
Aspron <124681044+Aspron@users.noreply.github.com>
2024-11-25 18:03:28 +0100

    Commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    println("Introduce la primera palabra:")
+    val palabra1 = readLine()?.trim() ?: ""
+    println("Introduce la segunda palabra:")
+    val palabra2 = readLine()?.trim() ?: ""
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    // Verificar si son anagramas
+    if (sonAnagramas(palabra1, palabra2)) {
+        println("Las palabras \"$palabra1\" y \"$palabra2\" son anagramas.")
+    } else {
+        println("Las palabras \"$palabra1\" y \"$palabra2\" no son anagramas.")
     }
+}
+
+fun sonAnagramas(palabra1: String, palabra2: String): Boolean {
+
+    if (palabra1.equals(palabra2, ignoreCase = true)) return false
+
+    return palabra1.lowercase().toCharArray().sorted() == palabra2.lowercase().toCharArray().sorted()
 }
