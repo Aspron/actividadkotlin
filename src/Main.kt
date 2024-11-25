563a3f5ce0059fcfec405493e073a27abd3c04ce
Aspron <124681044+Aspron@users.noreply.github.com>
2024-11-25 18:25:03 +0100

    Commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,18 @@
 
 fun main() {
+
     for (i in 1..100) {
+
         when {
+
             i % 3 == 0 && i % 5 == 0 -> println("triqui")
+
             i % 3 == 0 -> println("tri")
+
             i % 5 == 0 -> println("qui")
+
             else -> println(i)
+
         }
     }
 }
